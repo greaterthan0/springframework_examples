1edfd2610b7b3d15536a52f07037e8c5e0fd10bf
Greater than 0 <contactgreaterthan0@gmail.com>
2018-12-15 22:51:57 +0530

    Spring boot example added

---
+++ 01springboot/src/main/java/demos/springboot/FirstSpringBootDemo.java
@@ -0,0 +1,21 @@
+package demos.springboot;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@EnableAutoConfiguration
+public class FirstSpringBootDemo {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hi World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(FirstSpringBootDemo.class, args);
+    }
+}
