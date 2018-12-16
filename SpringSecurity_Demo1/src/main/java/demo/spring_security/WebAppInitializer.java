9759c40e2af3e00eacb1715fc5edf96abc2d875a
Greater than 0 <contactgreaterthan0@gmail.com>
2018-12-16 09:59:15 +0530

    Spring Security Demo - Minimum Configured Spring MVC example

---
+++ SpringSecurity_Demo1/src/main/java/demo/spring_security/WebAppInitializer.java
@@ -0,0 +1,22 @@
+package demo.spring_security;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+
+public class WebAppInitializer implements WebApplicationInitializer {
+
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+		ctx.setServletContext(servletContext);
+		ctx.register(AppConfig.class);
+		ServletRegistration.Dynamic servlet = servletContext.addServlet("dispatcher", new DispatcherServlet(ctx));
+		servlet.setLoadOnStartup(1);
+		servlet.addMapping("/");
+	}
+}
---
+++ SpringSecurity_Demo1/src/main/java/demo/spring_security/WelcomeController.java
@@ -0,0 +1,13 @@
+package demo.spring_security;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class WelcomeController {
+	@RequestMapping("/")
+	public @ResponseBody String welcome() {
+		return "Welcome to Spring MVC. This is example without using xml based configurations  ..";
+	}
+}
